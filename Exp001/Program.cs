11ea3d5b50c95a4199718d4eba382d38cf719d2f
Катерина <katttum@ya.ru>
2023-06-26 14:30:40 +0300

    среддняя цифра в 3 зн числе

--- Exp001/Program.cs
+++ Exp001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру 
+//этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n / 10;
+int b = a % 10;
+Console.WriteLine("Средняя цифра: " +b);
+
+
+
+
